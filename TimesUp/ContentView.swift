d984b006720208cefdbc0abd4449bd53436792d5
riskycheng <riskycheng@gmail.com>
2024-07-05 23:52:45 +0800

    initial project built with SwiftUI

---
+++ TimesUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimesUp
+//
+//  Created by Jian Cheng on 2024/7/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimesUp/TimesUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesUpApp.swift
+//  TimesUp
+//
+//  Created by Jian Cheng on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct TimesUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
